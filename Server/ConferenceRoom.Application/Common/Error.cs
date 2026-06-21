2287f270420f3e609f21fb56119eda76d8d45004
Muhammad <mmotala44@gmail.com>
2026-06-21 22:58:46 +0200

    Added Error and result types

---
+++ Server/ConferenceRoom.Application/Common/Error.cs
@@ -0,0 +1,18 @@
+﻿public sealed record Error(
+    string Code,
+    string Message,
+    ErrorType Type)
+{
+    public static readonly Error None = new(
+        string.Empty,
+        string.Empty,
+        ErrorType.None);
+
+    public static Error Validation(string message)
+    {
+        return new Error(
+            "Validation.Failed",
+            message,
+            ErrorType.Validation);
+    }
+}
---
+++ Server/ConferenceRoom.Application/Common/Results.cs
@@ -0,0 +1,48 @@
+﻿public class Result
+{
+    protected Result(bool isSuccess, Error error)
+    {
+        IsSuccess = isSuccess;
+        Error = error;
+    }
+
+    public bool IsSuccess { get; }
+    public bool IsFailure => !IsSuccess;
+    public Error Error { get; }
+
+    public static Result Success()
+    {
+        return new Result(true, Error.None);
+    }
+
+    public static Result Failure(Error error)
+    {
+        return new Result(false, error);
+    }
+
+    public static Result<TValue> Success<TValue>(TValue value)
+    {
+        return new Result<TValue>(value, true, Error.None);
+    }
+
+    public static Result<TValue> Failure<TValue>(Error error)
+    {
+        return new Result<TValue>(default, false, error);
+    }
+}
+
+public sealed class Result<TValue> : Result
+{
+    private readonly TValue? _value;
+
+    internal Result(TValue? value, bool isSuccess, Error error)
+        : base(isSuccess, error)
+    {
+        _value = value;
+    }
+
+    public TValue Value =>
+        IsSuccess
+            ? _value!
+            : throw new InvalidOperationException("Cannot access the value of a failed result.");
+}
